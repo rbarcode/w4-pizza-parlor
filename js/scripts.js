dc0f80be22c06a2eaea4c59a00ba11f97f26b9a8
Richard Barbour <richyb07@gmail.com>
2023-08-25 13:51:50 -0700

    Grab pizza size value from user input and assign correct value in variable.

--- js/scripts.js
+++ js/scripts.js
@@ -30,25 +30,57 @@ Pizza.prototype.calcPrice = function () {
 
 // UI Logic
 
-window.addEventListener("load", submit);
-window.addEventListener("reset", resubmit);
+window.addEventListener("load", userActions);
+// window.addEventListener("reset", resubmit);
+
+function userActions() {
+  submit();
+  click();
+}
 
 function submit() {
-  const form = document.getElementById("talk-to-robot");
+  const form = document.getElementById("pizza-order");
   form.addEventListener("submit", handleFormSubmission);
 }
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  
+
 }
 
-function resubmit() {
+function click() {
+  const div = document.getElementById("click-size-selection");
+  div.addEventListener("click", createNewOrder);
+  // const delButton = document.querySelector("button.delete");
+  // delButton.addEventListener("click", handleDelete);
+}
 
-  if (document.getElementById("submit-button").disabled === true) {
-    const p = document.getElementById("new-p");
-    p.remove();
+function createNewOrder() {
+  let pizzaSize;
+
+  if (event.target.value === "small") {
+    pizzaSize = "Small";
+    // console.log("Small was selected");
+  } else if (event.target.value === "medium") {
+    pizzaSize = "Medium";
+    // console.log("Medium was selected");
+  } else if (event.target.value === "large") {
+    pizzaSize = "Large";
+    // console.log("Large was selected");
   }
 
-  document.getElementById("submit-button").disabled = false;
+  // console.log("This was the size that was selected :" + pizzaSize);
+  // let newPizza = new Pizza(pizzaSize);
+  // return newPizza;
 }
+
+
+// function resubmit() {
+
+//   if (document.getElementById("submit-button").disabled === true) {
+//     const p = document.getElementById("new-p");
+//     p.remove();
+//   }
+
+//   document.getElementById("submit-button").disabled = false;
+// }
