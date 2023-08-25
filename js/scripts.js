bfd8dd79c5a3193742961ce5c225175f051da5c5
Richard Barbour <richyb07@gmail.com>
2023-08-25 10:04:14 -0700

    Pass test 5.

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,18 @@
 // Pizza Constructor----
 
 function Pizza(size, toppings = []) {
-	this.size = size;
-	this.toppings = toppings;
+  this.size = size;
+  this.toppings = toppings;
 }
 
-Pizza.prototype.calcPrice = function() {
+Pizza.prototype.calcPrice = function () {
   let basePrice;
   let toppingsPrice = .50;
   let costOfToppings = this.toppings.length * toppingsPrice;
   if (this.size === "Large") {
     basePrice = 12.50;
+  } else if (this.size === "Medium") {
+    basePrice = 10;
   }
   return basePrice;
-  }
+}
