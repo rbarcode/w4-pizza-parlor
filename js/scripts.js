3524dc045ca8052d2e68669032b8a5ba26290d1c
Richard Barbour <richyb07@gmail.com>
2023-08-25 09:18:40 -0700

    Pass test 2.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 // Business Logic
 
-// Business Logic for Pizza ----
+// Pizza Constructor----
 
 function Pizza(size, toppings) {
 	this.size = size;
 	this.toppings = toppings;
 }
+
+Pizza.prototype.calcPrice = function() {
+
+  let toppingsPrice = .50;
+  return toppingsPrice;
+  }
